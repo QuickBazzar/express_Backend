9a323f9556d5318b73c766e234018c4ca0a10458
Abhishek <abhishekpagar2020@gmail.com>
2025-12-15 03:19:14 +0530

    order and oredritem module completed

--- server.js
+++ server.js
@@ -4,14 +4,23 @@ const cors = require('cors')
 
 //userdefined modules
 const authorizeUser = require('./utils/authuser')
+
 const userRouter = require('./routes/user.js')
 const retailerRouter = require('./routes/retailers.js')
+const orderRouter = require('./routes/order.js')
+const orderItemRouter = require('./routes/orderItem.js')
+
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(authorizeUser)
+
 app.use('/user', userRouter)
 app.use('/retailer', retailerRouter)
+app.use('/orders',orderRouter)
+app.use('/orderitem',orderItemRouter)
+
+
 app.listen(4000, 'localhost', ()=> {
     console.log('Server Started At Port 4000')
 })
